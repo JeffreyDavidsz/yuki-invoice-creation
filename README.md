1da3582a00d85b4b0631703aa6d6b9fc86cb3418
Jeffrey Davidsz <jeffrey.davidsz@vicreo.eu>
2023-08-07 16:41:06 +0200

    create seperate module

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # yuki-invoice-creation
+
+https://support.yuki.nl/en/support/solutions/articles/80000787298-sales-functional-description-of-fields-sales-invoices-xml
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 require("dotenv").config(); // load environment variables from .env file
-const { JSDOM } = require("jsdom");
-const strAccessKey = process.env.API_KEY;
-const strAdministrationId = process.env.ADMINISTRATION_ID;
-const strWebserviceUrl = "https://api.yukiworks.nl/ws/Sales.asmx";
+const { Authenticate, ProcessSalesInvoices } = require("./yuki_api");
+const strAccessKey = process.env.API_KEY; // API_KEY from .env file
+const strAdministrationId = process.env.ADMINISTRATION_ID; // ADMINISTRATION_ID from .env file
 const strSalesXml = `
 <SalesInvoices xmlns="urn:xmlns:http://www.theyukicompany.com:salesinvoices" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         <SalesInvoice>
                 <Reference>669EC934-0187</Reference>
-                <Subject>Testfactuur - 1</Subject>
+                <Subject>Testfactuur - 2</Subject>
                 <PaymentMethod>ElectronicTransfer</PaymentMethod>
                 <Process>false</Process>
                 <EmailToCustomer>false</EmailToCustomer>
                 <Layout>VICREO ENGELS</Layout>
-                <Date>2023-06-22</Date>
-                <DueDate>2023-07-22</DueDate>
+                <Date>2023-07-22</Date>
+                <DueDate>2023-08-22</DueDate>
                 <PriceList />
                 <Currency />
                 <Remarks>Stripe</Remarks>
@@ -57,88 +56,14 @@ const strSalesXml = `
 `;
 
 
-// Create an async function to perform the dynamic import
-async function performFetch() {
-  
-    // Build SOAP XML message for WebServiceMethod "http://www.theyukicompany.com/Authenticate".
-    const strXml = `<?xml version="1.0" encoding="utf-8"?>
-  <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-    <soap12:Body>
-      <Authenticate xmlns="http://www.theyukicompany.com/">
-        <accessKey>${strAccessKey}</accessKey>
-      </Authenticate>
-    </soap12:Body>
-  </soap12:Envelope>`;
-  
-    // Create ServerXMLHTTP object.
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/soap+xml; charset=utf-8",
-        SOAPAction: "http://www.theyukicompany.com/Authenticate",
-        "Content-Length": strXml.length.toString(),
-      },
-      body: strXml,
-    };
-  
-    try {
-      // Send SOAP message using the dynamically imported fetch
-      const response = await fetch(strWebserviceUrl, options);
-  
-      // Print out the request status.
-      console.log(
-        `WebServiceMethod "Authenticate" request status: ${response.status} ${response.statusText}\n\n`
-      );
-  
-      // Print out the SOAP XML response.
-      const responseText = await response.text();
-      console.log(`<div style="border: solid 1px black;">${responseText}</div>\n\n`);
-  
- // Use jsdom to parse the XML response for the second SOAP request
- const dom = new JSDOM(responseText, { contentType: "text/xml" });
- const xmlDoc = dom.window.document;
- const strSessionId = xmlDoc.querySelector("AuthenticateResult").textContent;
-  
-      // Build SOAP XML message for WebServiceMethod "http://www.theyukicompany.com/ProcessSalesInvoices".
-      const strXml2 = `<?xml version="1.0" encoding="utf-8"?>
-  <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-    <soap12:Body>
-      <ProcessSalesInvoices xmlns="http://www.theyukicompany.com/">
-        <sessionId>${strSessionId}</sessionId>
-        <administrationId>${strAdministrationId}</administrationId>
-        <xmlDoc>${strSalesXml}</xmlDoc>
-      </ProcessSalesInvoices>
-    </soap12:Body>
-  </soap12:Envelope>`;
-  
-      // Create ServerXMLHTTP object for the second request.
-      const options2 = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/soap+xml; charset=utf-8",
-          SOAPAction: "http://www.theyukicompany.com/ProcessSalesInvoices",
-          "Content-Length": strXml2.length.toString(),
-        },
-        body: strXml2,
-      };
-  
-      // Send the second SOAP message using the dynamically imported fetch
-      const response2 = await fetch(strWebserviceUrl, options2);
-  
-      // Print out the request status.
-      console.log(
-        `WebServiceMethod "ProcessSalesInvoices" request status: ${response2.status} ${response2.statusText}\n\n`
-      );
-  
-      // Print out the SOAP XML response.
-      const responseText2 = await response2.text();
-      console.log(`<div style="border: solid 1px black;">${responseText2}</div>`);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-  
-  // Call the function to start the dynamic import and execute the SOAP requests
-  performFetch();
+/**
+ * Deliver an invoice to Yuki
+ */
+async function DeliverInvoice() {
+  const sessionId = await Authenticate(strAccessKey); // Get the sessionID
+  console.log(
+    await ProcessSalesInvoices(sessionId, strAdministrationId, strSalesXml)
+  );
+}
 
-
+DeliverInvoice();
